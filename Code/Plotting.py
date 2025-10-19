b9d0da54755df31f00359dd1a8ac068062e8c206
Luis Gindorf <luis.gindorf@gmail.com>
2025-10-19 17:42:59 +0200

    Added Plotting for pytorch tensor as map

--- Code/Plotting.py
+++ Code/Plotting.py
@@ -1,8 +1,26 @@
 from matplotlib import pyplot as plt
+import xarray as xr
+import torch
 
 
-def plot_map(data, show=True, save_name=None):
+def plot_xarray_as_map(data, show=True, save_name=None):
+    assert isinstance(data, xr.Dataset)
     data.to_dataarray().plot()
+    if save_name is not None:
+        path = "Figures/" + save_name + ".pdf"
+        print(path)
+        plt.savefig(path)
+    if show:
+        plt.show()
+
+
+def plot_tensor_as_map(data, show=True, save_name=None):
+    assert isinstance(data, torch.Tensor)
+    data = data.cpu().numpy()
+    plt.imshow(data)
+    plt.xlabel("Latitude")
+    plt.ylabel("Longitude")    
+    plt.colorbar()
     if save_name is not None:
         path = "Figures/" + save_name + ".pdf"
         print(path)
