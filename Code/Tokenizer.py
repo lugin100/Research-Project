154924ba2a2f5c66171720a9c4e62ec1a8d4a2ee
Luis Gindorf <luis.gindorf@gmail.com>
2025-10-27 17:13:21 +0100

    Wrote KMeans clustering as Tokenizer

--- Code/Tokenizer.py
+++ Code/Tokenizer.py
@@ -1,10 +1,11 @@
-from typing import Callable
-
+import numpy as np
+import torch
 import torch.nn.functional as F
 from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
 from torch import nn
 from torch.utils.data import DataLoader
+from triton.language import float16
 
 from Code.CoeffDataset import CoeffDataset
 from Code.Functions import *
@@ -45,79 +46,28 @@ class AutoencoderTokenizer(nn.Module):
         else:
             return F.softmax(gumbel_logits, dim=-1)
 
+class ClusteringTokenizer:
+
+    def __init__(self, train_data, V: int):
+        self.clustering = KMeans(n_clusters=V).fit(train_data.cpu())
+
+    def parse_array(self, array):
+        if isinstance(array, torch.Tensor):
+            return array.to(torch.float16).cpu().numpy()
+        if isinstance(array, np.ndarray):
+            return array.astype(float)
+        else:
+            raise TypeError("Must pass np array or torch tensor")
 
 
-model = AutoencoderTokenizer(dim_in = 2, V=500).to(DEVICE)
-model.load_state_dict(torch.load('Models/tokenizer-gumbel-weights.pth', weights_only=True))
-model.train()
+    def tokenize(self, coeffs):
+        coeffs = self.parse_array(coeffs)
+        print(coeffs.dtype)
+        return self.clustering.predict(coeffs)
 
-LEARN_RATE = 5e-3
-B = 50
-EPOCHS = 20
-dataset = CoeffDataset("data/coeffs_wind_speed_level=500_time=slice('1970', '1972').pt")
-dataloader = DataLoader(dataset, batch_size=B, shuffle=True)
 
-loss_fn = torch.nn.MSELoss(reduction="mean")
+    def detokenize(self, tokens):
+        tokens = self.parse_array(tokens)
+        return self.clustering.cluster_centers_[tokens]
 
-optimizer = torch.optim.Adam(model.parameters(), lr=LEARN_RATE)
 
-def model_fn(input):
-    one_hot = model.encode_hard(input).float()
-    return model.decode(one_hot)
-
-def evaluate(f: Callable):
-    model.eval()
-    with torch.no_grad():
-        test_loss = 0
-        for batch in dataloader:
-            _input = flatten_coeffs(batch)
-            (B,T) = _input.shape
-            _input = torch.view_as_real(_input).reshape(B * T, 2)
-            originals = _input
-            pred = f(_input)
-            test_loss += loss_fn(pred, originals).item()
-        test_loss /= len(dataloader)
-        print(test_loss)
-    model.train()
-
-def train():
-    for epoch in range(EPOCHS):
-        print(f"Epoch {epoch+1}")
-        for i, batch in enumerate(dataloader):
-            _input = flatten_coeffs(batch)
-            (B, T) = _input.shape
-            _input = torch.view_as_real(_input).reshape(B * T, 2)
-            originals = _input
-            intermediate = model.encode_soft(_input)
-            output = model.decode(intermediate)
-            #pred = torch.view_as_complex(output).reshape(B, T)
-            loss = loss_fn(output, originals)
-
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-        evaluate()
-
-#train()
-#evaluate()
-#torch.save(model.state_dict(), 'Models/tokenizer-gumbel-weights-v2.pth')
-
-coeff_batch = torch.view_as_real(next(iter(dataloader)))
-coeff_batch = flatten_coeffs(coeff_batch).reshape((-1,2))
-reals = coeff_batch[...,0].cpu()
-imags = coeff_batch[...,1].cpu()
-
-clustering = KMeans(n_clusters=500).fit(coeff_batch.cpu())
-ys = clustering.labels_
-#ys = model.encode_hard(coeff_batch).argmax(dim=-1).cpu()
-plt.scatter(reals, imags, marker="x", alpha=0.5, c=ys, cmap="tab20")
-plt.xlabel("Re")
-plt.ylabel("Im")
-plt.colorbar()
-plt.show()
-
-def cluster_fn(input):
-    indices = clustering.predict(input.cpu())
-    return torch.tensor(clustering.cluster_centers_[indices]).to(DEVICE)
-
-evaluate(cluster_fn)
