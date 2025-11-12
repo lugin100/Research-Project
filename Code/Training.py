79e318c62f1cd9d7d9e1f2855bfeb40159b4012d
Luis Gindorf <luis.gindorf@student.uni-tuebingen.de>
2025-11-12 18:56:23 +0100

    Added LR scheduler

--- Code/Training.py
+++ Code/Training.py
@@ -18,9 +18,7 @@ params = {
 	"EPS": 1e-5, 		# Clamping constant for variance of predicted distriutions
 	"D": 512,  			# Embedding dimension
 	"H": 4,  			# Number of heads in multi-head attention
-	"NORM_FIRST": True, # Whether to apply layer norm first or after attention and feedforward
-	"LR": 5e-3, 		# Learning Rate
-    "MAX_EPOCHS:" 50   # Maximal number of training epochs
+	"NORM_FIRST": True  # Whether to apply layer norm first or after attention and feedforward
 }
 
 params["T"] = triangular_number(params["N"]) # Number of coefficients given in training
@@ -45,13 +43,51 @@ validloader = DataLoader(ds, batch_size=params["B"], num_workers=7, shuffle=Fals
 
 ############# Optimizer #############
 from types import MethodType
+from torch.optim.lr_scheduler import LinearLR, CosineAnnealingLR, SequentialLR
 
-params["LR"] = 5e-3 # Learning Rate
-params["MAX_EPOCHS"] = 50
+params.update({
+	"BASE_LR": 5e-3,	 		# Base Learning Rate
+	"LR_START_FACTOR": 1e-8,	# LR factor for first warmup step
+    "MAX_EPOCHS": 50,    		# Maximal number of training epochs
+    "WARMUP_DURATION": 0.1, 	# Fraction of epochs until warmup completion
+	"MIN_LR": 1e-7				# Learning rate at last cosine step
+	})
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=parms["LR"])
-configure_optimizers = lambda self: optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr=params["LR"])
+
+total_steps =  params["MAX_EPOCHS"] * len(trainloader)
+warmup_steps = int(params["WARMUP_DURATION"] * total_steps)
+cosine_steps = total_steps - warmup_steps
+
+warmup_schedule = LinearLR(
+	optimizer,
+	start_factor=params["LR_START_FACTOR"],
+	end_factor=1,
+	total_iters=warmup_steps)
+cosine_schedule = CosineAnnealingLR(
+	optimzer,
+	T_max=cosine_steps,
+	eta_min=params["MIN_LR"]
+	)
+scheduler = SequentialLR(
+            optimizer,
+            schedulers=[warmup_schedule, cosine_schedule],
+            milestones=[params["warmup_steps"]]
+        )
+
+def configure_optimizers(self):
+	return {
+		"optimizer": optimizer,
+		"lr_scheduler": {
+			"scheduler": scheduler,
+			"interval": "step"
+		}}
 model.configure_optimizers = MethodType(configure_optimizers, model)
+
+def on_train_epoch_start(self):
+	cur_lr = self.trainer.optimizers[0].param_groups[0]["lr"]
+	self.log("LR", cur_lr, prog_bar=True, on_step=True, on_epoch=False)
+model.on_train_epoch_start = MethodType(on_train_epoch_start, model)
 
 
 ############# Logging ###############
@@ -59,9 +95,10 @@ from lightning.pytorch.loggers import WandbLogger
 
 wandb_logger = WandbLogger(project="autoregressive-downcasting", name=RUN_NAME, log_model="all")
 wandb_logger.experiments.config.update(params)
+
+
 ############# Execution #############
 from lightning import Trainer
-
 
 trainer = Trainer(max_epochs=params["MAX_EPOCHS"], logger=wandb_logger, log_every_n_steps=1)
 trainer.fit(model, trainloader, validloader)
