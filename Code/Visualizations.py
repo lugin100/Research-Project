70fa592a3337fddea51c1edafc353ac5eee5b720
Luis Gindorf <lugin100@github.com>
2025-11-20 15:56:19 +0100

    Fixed errors in visualization, produced plots

--- Code/Visualizations.py
+++ Code/Visualizations.py
@@ -1,19 +1,19 @@
 from Plotting import *
 from Dataset import WeatherDataset
-from Functons import *
+from Functions import *
 
 natural_ds = WeatherDataset("wind_speed", time_slice=slice("1970-01-01", "1970-01-02", None), level=500)
 
 single_datapoint = natural_ds.__getitem__(4)
 plot_tensor_as_map(single_datapoint, show=False, save_name="Ground Truth")
 
-single_datapoint_coeffs = flatten_coeffs(sh_transform(single_datapoint))
+single_datapoint_coeffs = flatten_coeffs(sh_transform(single_datapoint[None,...])).squeeze()
 
 print(single_datapoint_coeffs.shape)
 # Mask coeffs to be predicted
-L = 60*61/2
+L = int(60*61/2)
 single_datapoint_coeffs[L:] = 0
 
-input_datapoint = inv_sh_transform(single_datapoint_coeffs)
+input_datapoint = inv_sh_transfrom(unflatten_coeffs(single_datapoint_coeffs[None,...]))
 
-plot_tensor_as_map(input_datapoint, show=False, save_name="Model input")
+plot_tensor_as_map(input_datapoint.squeeze(), show=False, save_name="Model input")
