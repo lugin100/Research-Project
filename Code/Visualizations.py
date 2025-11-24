f7af7663869512098b7a1eaefc536b82d0f478c7
Luis Gindorf <luis.gindorf@student.uni-tuebingen.de>
2025-11-24 16:03:18 +0100

    Removed spaces from figure names

--- Code/Visualizations.py
+++ Code/Visualizations.py
@@ -7,7 +7,7 @@ natural_ds = WeatherDataset("wind_speed", time_slice=slice("1970-01-01", "1970-0
 
 # Plot single datapoint as example
 single_datapoint = natural_ds.__getitem__(4)
-plot_tensor_as_map(single_datapoint, show=False, save_name="Ground Truth")
+plot_tensor_as_map(single_datapoint, show=False, save_name="Ground-Truth")
 
 # Transform to coefficients
 single_datapoint_coeffs = flatten_coeffs(sh_transform(single_datapoint[None,...])).squeeze()
@@ -18,9 +18,9 @@ single_datapoint_coeffs[L+1:] = 0
 
 # Plot training model input (coefficients zeroed after L)
 input_datapoint = inv_sh_transfrom(unflatten_coeffs(single_datapoint_coeffs[None,...]))
-plot_tensor_as_map(input_datapoint.squeeze(), show=False, save_name="Training model input")
+plot_tensor_as_map(input_datapoint.squeeze(), show=False, save_name="Training-model-input")
 
 # Plot inference model input (only coefficients up to L)
 single_datapoint_coeffs = single_datapoint_coeffs[:L]
 input_datapoint = inv_sh_transfrom(unflatten_coeffs(single_datapoint_coeffs[None,...]))
-plot_tensor_as_map(input_datapoint.squeeze(), show=False, save_name="Inference model input")
+plot_tensor_as_map(input_datapoint.squeeze(), show=False, save_name="Inference-model-input")
