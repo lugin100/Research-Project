2212030950373dd2d5a42e9808d06767f9d550e9
Luis Gindorf <luis.gindorf@student.uni-tuebingen.de>
2025-12-08 12:49:52 +0100

    Refactor Visualizations for full model

--- Code/Visualizations.py
+++ Code/Visualizations.py
@@ -1,8 +1,16 @@
 import torch
-from Plotting import *
+from Plotting import plot_tensor_as_map
 from Dataset import WeatherDataset
-from Functions import *
+from Functions import (
+		DEVICE,
+		flatten_coeffs,
+		unflatten_coeffs,
+		sh_transform,
+		inv_sh_transform)
+
 from Transformer import LightningModel
+
+model_str = "6ybji6ws"
 
 natural_ds = WeatherDataset("wind_speed", time_slice=slice("1970-01-01", "1970-01-02", None), level=500)
 
@@ -16,10 +24,8 @@ plot_tensor_as_map(single_datapoint, show=False, save_name="Ground-Truth")
 # Transform to coefficients
 single_datapoint_coeffs = flatten_coeffs(sh_transform(single_datapoint[None,...]))
 
-# Mask coeffs to be predicted
-single_datapoint_coeffs[:,L+1:] = 0
-
 # Plot training model input (coefficients zeroed after L)
+single_datapoint_coeffs[:,L+1:] = 0
 input_datapoint = inv_sh_transform(unflatten_coeffs(single_datapoint_coeffs))
 plot_tensor_as_map(input_datapoint.squeeze(), show=False, save_name="Training-model-input")
 
@@ -32,15 +38,18 @@ plot_tensor_as_map(input_datapoint.squeeze(), show=False, save_name="Inference-m
 # Load model checkpoint
 from Transformer import LightningModel
 
-#path = "autoregressive-downcasting/vsn5fk32/checkpoints/best-model.ckpt"
-path = "autoregressive-downcasting/6ybji6ws/checkpoints/best-model.ckpt"
+path = f"autoregressive-downcasting/{model_str}/checkpoints/best-model.ckpt"
 
 model = LightningModel.load_from_checkpoint(path)
 model.eval()
-model.freeze()
 model.to(DEVICE)
-# Infere and plot prediction
-assert False, "Does not work wih current model because it has smaller input resolution"
-raw_pred = model.infer(torch.view_as_real(single_datapoint_coeffs))
-pred = inv_sh_transform(unflatten_coeffs(torch.view_as_complex(raw_pred)))
+
+means = torch.load("data/wind-speed_level-500_testset_means.pt", weights_only=True).to(DEVICE)
+stds = torch.load("data/wind-speed_level-500_testset_stds.pt", weights_only=True).to(DEVICE)
+
+# Infer and plot prediction
+model_input  = (single_datapoint_coeffs - means) / stds
+raw_pred = model.infer(torch.view_as_real(model_input))
+rescaled_pred = raw_pred * stds + means
+pred = inv_sh_transform(unflatten_coeffs(torch.view_as_complex(rescaled_pred)))
 plot_tensor_as_map(pred.squeeze(), show=False, save_name="Inference-model-output")
