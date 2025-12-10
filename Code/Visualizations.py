0c4ef0ee9c901db6936d849784ac96bba12ee7b6
Luis Gindorf <luis.gindorf@student.uni-tuebingen.de>
2025-12-10 20:10:21 +0100

    Create ground truth data only once in own results folder

--- Code/Visualizations.py
+++ Code/Visualizations.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from Plotting import plot_tensor_as_map
 from Dataset import WeatherDataset
@@ -13,45 +14,33 @@ from Transformer import LightningModel
 model_str = "mitogrw5"
 DIR = f"Results/{model_str}/"
 
-natural_ds = WeatherDataset("wind_speed", time_slice=slice("2011", "2012", None), level=500)
+ground_truth_path = "Results/ground-truth"
+if not os.path.exists(ground_truth_path):
+    os.makedirs(ground_truth_path)
+	
+	natural_ds = WeatherDataset("wind_speed", time_slice=slice("2011", "2012", None), level=500)
 
-# Works for full size model
-L = int(60*61/2)
+	# Plot single datapoint as example
+	single_datapoint = natural_ds.__getitem__(0)
+	plot_tensor_as_map(single_datapoint, show=False, save_name=ground_truth_path + "Ground-Truth")
 
-# Plot single datapoint as example
-single_datapoint = natural_ds.__getitem__(0)
-plot_tensor_as_map(single_datapoint, show=False, save_name=DIR + "Ground-Truth")
+	# Transform to coefficients
+	single_datapoint_coeffs = flatten_coeffs(sh_transform(single_datapoint[None,...]))
 
-# Transform to coefficients
-single_datapoint_coeffs = flatten_coeffs(sh_transform(single_datapoint[None,...]))
+	L = int(60*61/2)
+	# Plot training model input (coefficients zeroed after L)
+	single_datapoint_coeffs[:,L:] = 0
+	input_datapoint = inv_sh_transform(unflatten_coeffs(single_datapoint_coeffs))
+	plot_tensor_as_map(input_datapoint.squeeze(), show=False, save_name=ground_truth_path + "Training-model-input")
 
-# Plot training model input (coefficients zeroed after L)
-single_datapoint_coeffs[:,L+1:] = 0
-input_datapoint = inv_sh_transform(unflatten_coeffs(single_datapoint_coeffs))
-plot_tensor_as_map(input_datapoint.squeeze(), show=False, save_name=DIR + "Training-model-input")
-
-# Plot inference model input (only coefficients up to L)
-single_datapoint_coeffs = single_datapoint_coeffs[:,:L]
-input_datapoint = inv_sh_transform(unflatten_coeffs(single_datapoint_coeffs))
-plot_tensor_as_map(input_datapoint.squeeze(), show=False, save_name=DIR + "Inference-model-input")
+	# Plot inference model input (only coefficients up to L)
+	single_datapoint_coeffs = single_datapoint_coeffs[:,:L]
+	input_datapoint = inv_sh_transform(unflatten_coeffs(single_datapoint_coeffs))
+	plot_tensor_as_map(input_datapoint.squeeze(), show=False, save_name=ground_truth_path + "Inference-model-input")
 
 
-# Load model checkpoint
-from Transformer import LightningModel
-
-path = f"autoregressive-downcasting/{model_str}/checkpoints/best-model.ckpt"
-
-model = LightningModel.load_from_checkpoint(path)
-model.eval()
-model.to(DEVICE)
-
-means = torch.load("data/wind-speed_level-500_testset_means.pt", weights_only=True).to(DEVICE)
-stds = torch.load("data/wind-speed_level-500_testset_stds.pt", weights_only=True).to(DEVICE)
-
-# Infer and plot prediction
-model_input  = (single_datapoint_coeffs - means[:L]) / stds[:L]
-raw_pred = model.infer(torch.view_as_real(model_input))
-print(raw_pred.shape)
+# Load prediction
+torch.load()
 T = 60*121
 rescaled_pred = torch.view_as_complex(raw_pred.squeeze()) * stds[:T] + means[:T]
 pred = inv_sh_transform(unflatten_coeffs(rescaled_pred[None,:]))
