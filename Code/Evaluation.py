95fb1073ee877bd44eadfab6f022cf565f173306
Luis Gindorf <luis.gindorf@student.uni-tuebingen.de>
2025-12-11 15:52:59 +0100

    Evaluation for model

--- Code/Evaluation.py
+++ Code/Evaluation.py
@@ -3,37 +3,43 @@ import torch
 
 from Metrics import RMSE
 from glob import glob
-from Dataset import WeatherDataset
+from Functions import triangular_number, inv_sh_transform
+from Dataset import WeatherDataset, CoeffDataset
 from torch.utils.data import DataLoader
 
 
-model_str = "interpolation"
-
+model_str = "mitogrw5"
+data_path = "data/wind-speed_level-500_testset"
 log_path = f"Results/{model_str}/Evaluation.txt"
 
 pred_path = f"Results/{model_str}/Predictions/reals/"
-print(pred_path)
+
 pred_files = sorted(glob(pred_path + "batch_*.pt"))
 
-B = 256
-ground_truth_ds = WeatherDataset("wind_speed", time_slice=slice("2011", "2022", None), level=500)
+B = 5
+#ground_truth_ds = WeatherDataset("wind_speed", time_slice=slice("2011", "2022", None), level=500)
+ground_truth_ds = CoeffDataset(data_path = "data/wind-speed_level-500_testset")
 ground_truth_dl = DataLoader(ground_truth_ds, batch_size=B, num_workers=7, shuffle=False)
+
 
 assert len(ground_truth_dl) == len(pred_files)
 
+T = triangular_number(120)
 with open(log_path, "w") as log_file:
 	with torch.no_grad():
 
 		rmses = []
 
-		for pred_path, ground_truth in zip(pred_files, ground_truth_dl):
-			batch = torch.load(pred_path)
-			if batch.shape[0] != B:
+		for pred_path, gt_batch in zip(pred_files, ground_truth_dl):
+			pred_batch = torch.load(pred_path)
+			if pred_batch.shape[0] != B:
 				break
-			assert batch.shape == ground_truth.shape
 
+			gt_batch = gt_batch[:,:T]
+			gt_reals_batch = inv_sh_transform(gt_batch)
+			assert pred_batch.shape == gt_reals_batch.shape
 
-			rmse = RMSE(batch, ground_truth, feature_dims=(-1,-2), reduce=True)
+			rmse = RMSE(pred_batch, gt_reals_batch, feature_dims=(-1,-2), reduce=True)
 			rmse = rmse.cpu().item()
 			rmses.append(rmse)
 
