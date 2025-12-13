17d2ba0e286165ce51e7bbf4db0db13ea401d9d8
Luis Gindorf <luis.gindorf@student.uni-tuebingen.de>
2025-12-13 07:46:56 +0100

    Evaluation with mapping of predictions to correct ground truth samples

--- Code/Evaluation.py
+++ Code/Evaluation.py
@@ -8,7 +8,7 @@ from Dataset import CoeffDataset
 from torch.utils.data import DataLoader
 from Plotting import plot_coeffs_as_img
 
-model_str = "interpolation"
+model_str = "mitogrw5"
 data_path = "data/wind-speed_level-500_testset"
 log_path = f"Results/{model_str}/"
 
@@ -23,16 +23,16 @@ real_pred_files = sorted(real_pred_files, key=sort)
 coeff_pred_files = Path(pred_path).glob("coeffs/batch_*.pt")
 coeff_pred_files = sorted(coeff_pred_files, key=sort)
 
-B = 256
+B = 5
 #ground_truth_ds = WeatherDataset("wind_speed", time_slice=slice("2011", "2022", None), level=500)
 ground_truth_ds = CoeffDataset("data/wind-speed_level-500_testset")
-ground_truth_dl = DataLoader(ground_truth_ds, batch_size=B, num_workers=7, shuffle=False)
+#ground_truth_dl = DataLoader(ground_truth_ds, batch_size=B, num_workers=7, shuffle=False)
 
 #print(len(ground_truth_dl))
 #print(len(pred_files))
 #assert len(ground_truth_dl) == len(pred_files)
 
-T = triangular_number(121)
+T = triangular_number(120)
 
 means = torch.load("data/wind-speed_level-500_testset_means.pt", weights_only=True)[None,:T].to(DEVICE)
 stds = torch.load("data/wind-speed_level-500_testset_stds.pt", weights_only=True)[None,:T].to(DEVICE)
@@ -42,6 +42,7 @@ with open(log_path + "Evaluation.txt", "w") as log_file:
 
 		# RMSE of real data prediction
 		rmses = []
+		"""
 		for pred_path, gt_batch in zip(real_pred_files, ground_truth_dl):
 			pred_batch = torch.load(pred_path, weights_only=True).to(DEVICE)
 			if pred_batch.shape[0] != B:
@@ -55,6 +56,21 @@ with open(log_path + "Evaluation.txt", "w") as log_file:
 			rmse = RMSE(pred_batch, gt_reals_batch, feature_dims=(-1,-2), reduce=True)
 			rmse = rmse.cpu().item()
 			rmses.append(rmse)
+		"""
+		pred_index = 0
+		for pred_batch in real_pred_files:
+			pred_batch = torch.load(pred_path, weights_only=True).to(DEVICE)
+			for pred_item in pred_batch:
+				gt_index = 8 * pred_index
+				gt_item = ground_truth_ds.__getitem__(gt_index)
+				pred_index += 1
+				gt_item = gt_item[:,:T].to(DEVICE)
+				gt_item = gt_item * stds + means
+				gt_reals_item = unflatten_coeffs(inv_sh_transform(gt_item))
+				rmse = RMSE(pred_item, gt_reals_item, feature_dims=(-1,-2), reduce=True)
+				rmse = rmse.cpu().item()
+				rmses.append(rmse)
+			
 
 		print("RMSE between test weather dataset and predictions: ", sum(rmses)/len(rmses), file=log_file)
 
