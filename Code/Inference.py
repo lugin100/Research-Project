6780d86e06536116fc6709bcb05288e49b9d8867
Luis Gindorf <lugin100@github.com>
2025-12-15 16:06:49 +0100

    Set up inference for new model

--- Code/Inference.py
+++ Code/Inference.py
@@ -1,7 +1,7 @@
 import os
 import torch
 from types import MethodType
-from Transformer import LightningModel
+from HFTransformer import TransformerModel
 from lightning import Trainer
 from Dataset import CoeffDataset
 from torch.utils.data import DataLoader
@@ -17,7 +17,7 @@ torch.set_float32_matmul_precision("medium") # Faster on tensor cores
 model_str = "f0edygk8"
 DIR = f"Results/{model_str}/Predictions"
 L = triangular_number(61)
-
+T = triangular_number(121)
 # Load dataset
 data_path = "data/wind-speed_level-500_testset"
 testset = CoeffDataset(data_path, index_limit=L)
@@ -25,9 +25,9 @@ testset = CoeffDataset(data_path, index_limit=L)
 # Modify dataset such that it returns index too
 def __getitem__(self, idx):
 	if self.index_limit is None:
-		return idx, self.data[idx]
+		return torch.tensor(idx), self.data[idx]
 	else:
-		return idx, self.data[idx, :self.index_limit]
+		return torch.tensor(idx), self.data[idx, :self.index_limit]
 testset.__getitem__ = MethodType(__getitem__, testset)
 
 B = 32
@@ -37,7 +37,7 @@ dl = DataLoader(testset, batch_size=B, num_workers=7, shuffle=False)
 # Load model checkpoint
 model_path = f"autoregressive-downcasting/{model_str}/checkpoints/best-model.ckpt"
 
-model = LightningModel.load_from_checkpoint(model_path)
+model = TransformerModel.load_from_checkpoint(model_path)
 trainer = Trainer(
     accelerator="gpu",
     devices="auto",  # Uses all available GPUs
@@ -48,9 +48,10 @@ os.makedirs(os.path.dirname(DIR + "/coeffs"), exist_ok=True)
 os.makedirs(os.path.dirname(DIR + "/reals"), exist_ok=True)
 
 def predict_step(self, batch, batch_idx):
-	del batch_idx # This index is just a lie when using DDP
-	global_indices, batch = batch
-	batch = torch.view_as_real(batch[:,:L])
+	print( batch_idx) # This index is just a lie when using DDP
+	print(batch.shape)
+	global_indices, data = batch
+	batch = torch.view_as_real(data[:,:L])
 	pred_batch = self.infer(batch)
 	rescaled_preds = torch.view_as_complex(pred_batch) * stds + means
 	reals = inv_sh_transform(unflatten_coeffs(rescaled_preds))
