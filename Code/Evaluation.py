a047a72b704cccc9a7abc9fd8ce65228afdcabf0
Luis Gindorf <lugin100@github.com>
2025-12-29 17:08:55 +0100

    Configure evaluation for new model

--- Code/Evaluation.py
+++ Code/Evaluation.py
@@ -8,7 +8,7 @@ from Dataset import CoeffDataset
 from torch.utils.data import DataLoader
 from Plotting import plot_coeffs_as_img
 
-model_str = "random"
+model_str = "7vhmq3u0"
 data_path = "data/wind-speed_level-500_testset"
 log_path = f"Results/{model_str}/"
 
@@ -23,7 +23,7 @@ real_pred_files = sorted(real_pred_files, key=sort)
 coeff_pred_files = Path(pred_path).glob("coeffs/batch_*.pt")
 coeff_pred_files = sorted(coeff_pred_files, key=sort)
 
-B = 10000
+B = 64
 #ground_truth_ds = WeatherDataset("wind_speed", time_slice=slice("2011", "2022", None), level=500)
 ground_truth_ds = CoeffDataset("data/wind-speed_level-500_testset")
 ground_truth_dl = DataLoader(ground_truth_ds, batch_size=B, num_workers=7, shuffle=False)
