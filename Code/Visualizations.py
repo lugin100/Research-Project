c36e6f8cd4dec06201ee06cfb4a8948cf5cbd1b7
Luis Gindorf <lugin100@github.com>
2025-12-30 13:06:52 +0100

    Redid plots with fixed colorbar again

--- Code/Visualizations.py
+++ Code/Visualizations.py
@@ -33,7 +33,7 @@ if not os.path.exists(ground_truth_path):
 
 	# Plot single datapoint as example
 
-	plot_tensor_as_map(gt_reals, save_name=ground_truth_path + "Ground-Truth", vmin=None, vmax=None)
+	plot_tensor_as_map(gt_reals, save_name=ground_truth_path + "Ground-Truth", vmin=-30, vmax=45)
 
 	# Transform to coefficients
 	single_datapoint_coeffs = flatten_coeffs(sh_transform(gt_reals[None,...])).cpu()
@@ -43,12 +43,12 @@ if not os.path.exists(ground_truth_path):
 	single_datapoint_coeffs[:,L:] = 0
 	input_datapoint = inv_sh_transform(unflatten_coeffs(single_datapoint_coeffs)).squeeze()
 #	assert np.testing.assert_allclose(input_datapoint, gt_reals)
-	plot_tensor_as_map(input_datapoint, save_name=ground_truth_path + "Training-model-input", vmin=None, vmax=None)
+	plot_tensor_as_map(input_datapoint, save_name=ground_truth_path + "Training-model-input", vmin=-30, vmax=45)
 
 	# Plot inference model input (only coefficients up to L)
 	single_datapoint_coeffs = single_datapoint_coeffs[:,:L]
 	input_datapoint = inv_sh_transform(unflatten_coeffs(single_datapoint_coeffs))
-	plot_tensor_as_map(input_datapoint.squeeze(), save_name=ground_truth_path + "Inference-model-input", vmin=None, vmax=None)
+	plot_tensor_as_map(input_datapoint.squeeze(), save_name=ground_truth_path + "Inference-model-input", vmin=-30, vmax=45)
 
 
 # Load prediction
@@ -56,7 +56,7 @@ batch = torch.load(model_path + "Predictions/reals/batch_0.pt", weights_only=Tru
 first_sample = batch[0].cpu()
 
 lower_half = first_sample[61:,:]
-plot_tensor_as_map(first_sample, save_name=model_path + "model-output")
+plot_tensor_as_map(first_sample, save_name=model_path + "model-output", vmin=-30, vmax=45)
 
 diff = (first_sample - gt_reals) #[61:,:]
 plot_tensor_as_map(diff, save_name=model_path + "ground-truth-difference")
