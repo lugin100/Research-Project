46785dc7bd5a368953c1a0986ea591fe094ee145
Luis Gindorf <luis.gindorf@student.uni-tuebingen.de>
2025-12-30 12:49:53 +0100

    Fix bug that lead to plotting of wrong sample for visualization

--- Code/Visualizations.py
+++ Code/Visualizations.py
@@ -17,9 +17,6 @@ model_path = f"Results/{model_str}/"
 ground_truth_path = "Results/ground-truth/"
 data_path = "data/wind-speed_level-500_testset"
 
-natural_ds = WeatherDataset("wind_speed", time_slice=slice("2011", "2012", None), level=500)
-ground_truth = natural_ds.__getitem__(0)
-
 T = triangular_number(121)
 means = torch.load("data/wind-speed_level-500_testset_means.pt", weights_only=True)[None,:T]
 stds = torch.load("data/wind-speed_level-500_testset_stds.pt", weights_only=True)[None,:T]
@@ -35,16 +32,17 @@ if not os.path.exists(ground_truth_path):
 
 	# Plot single datapoint as example
 
-	plot_tensor_as_map(ground_truth, save_name=ground_truth_path + "Ground-Truth", vmin=0, vmax=70)
+	plot_tensor_as_map(gt_reals, save_name=ground_truth_path + "Ground-Truth", vmin=0, vmax=70)
 
 	# Transform to coefficients
-	single_datapoint_coeffs = flatten_coeffs(sh_transform(ground_truth[None,...]))
-
+	single_datapoint_coeffs = flatten_coeffs(sh_transform(gt_reals[None,...]))
+	assert torch.allclose(single_datapoint_coeffs, gt_coeff)
 	L = triangular_number(61)
 	# Plot training model input (coefficients zeroed after L)
 	single_datapoint_coeffs[:,L:] = 0
-	input_datapoint = inv_sh_transform(unflatten_coeffs(single_datapoint_coeffs))
-	plot_tensor_as_map(input_datapoint.squeeze(), save_name=ground_truth_path + "Training-model-input", vmin=0, vmax=70)
+	input_datapoint = inv_sh_transform(unflatten_coeffs(single_datapoint_coeffs)).squeeze()
+	assert torch.allclose(input_datapoint, gt_reals)
+	plot_tensor_as_map(input_datapoint, save_name=ground_truth_path + "Training-model-input", vmin=0, vmax=70)
 
 	# Plot inference model input (only coefficients up to L)
 	single_datapoint_coeffs = single_datapoint_coeffs[:,:L]
