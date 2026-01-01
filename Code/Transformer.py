f4372082fb9fe9c9552a0d754c9016bac1eb4310
Luis Gindorf <lugin100@github.com>
2026-01-01 15:31:49 +0100

    Evaluate True RMSE for validation by comparing to next token targets

--- Code/Transformer.py
+++ Code/Transformer.py
@@ -100,7 +100,7 @@ class TransformerModel(LightningModule):
 		pp_log = Metrics.perplexity(pis)
 		weighted_means = (pis[...,None] * means).sum(axis=-2)
 		rmse_log = Metrics.RMSE(means, torch.zeros_like(means), feature_dims=-1)
-		true_rmse_log = Metrics.RMSE(weighted_means, batch, feature_dims=-1)
+		true_rmse_log = Metrics.RMSE(weighted_means, target, feature_dims=-1)
 		variance_log = Metrics.variance_median(variances)
 		self.logs.append({"pi_log": pi_log, "pp_log": pp_log, "rmse_log": rmse_log, "true_rmse_log": true_rmse_log, "variance_log": variance_log, "loss_log": loss})
 
