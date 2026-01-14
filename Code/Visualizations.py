549c69657775efda5378db657601cdc660f643b8
Luis Gindorf <lugin100@github.com>
2026-01-14 17:34:52 +0100

    Generate sample visulization subplots

--- Code/Visualizations.py
+++ Code/Visualizations.py
@@ -1,6 +1,7 @@
 # Visualizations on first sample
 
 import numpy as np
+from matplotlib import pyplot as plt
 import torch
 from Dataset import CoeffDataset
 from Functions import (
@@ -20,34 +21,33 @@ coeff_ds = CoeffDataset(data_path)
 gt_coeff = coeff_ds.__getitem__(0)
 
 gt_coeff = (gt_coeff * stds + means)
-gt_reals = inv_sh_transform(unflatten_coeffs(gt_coeff)).squeeze().cpu()
+gt_reals = inv_sh_transform(unflatten_coeffs(gt_coeff[None])).squeeze().cpu()
 
 L = triangular_number(61)
 
 vmin = -30
 vmax = 40
 
-fig, subplots = plt.subplots(nrows = 4, ncols = 2, sharex="All", sharey="All")
+fig, subplots = plt.subplots(nrows = 4, ncols = 2, sharex="all", sharey="all")
 
 
 subplots[0,0].imshow(gt_reals, cmap = "turbo", vmin=vmin, vmax=vmax)
 
-gt_coeff_cutoff = gt_coeff[:,:L]
-gt_reals_input = inv_sh_transform(unflatten_coeffs(gt_coeff_cutoff)).squeeze()
+gt_coeff_cutoff = gt_coeff[:L]
+gt_reals_input = inv_sh_transform(unflatten_coeffs(gt_coeff_cutoff[None])).squeeze()
 
 subplots[0,1].imshow(gt_reals_input, cmap = "turbo", vmin=vmin, vmax=vmax)
 
 models = ["interpolation", "New-model", "New-model-deep"]
-for model in models:
-    batch = torch.load(f"Results/{model}/Predictions/reals/batch_0.pt", weights_only=True)
+for i, model in enumerate(models):
+    batch = torch.load(f"Results/{model}/Predictions/reals/batch_0.pt", weights_only=True, map_location="cpu")
     first_sample = batch[0].cpu()
     subplots[i+1,0].imshow(first_sample, cmap = "turbo", vmin=vmin, vmax=vmax)
-    
+
     difference = first_sample - gt_reals
-    subplots[i+1,1].imshow(difference, cmap = "turbo", vmin=vmin, vmax=vmax)
-    
-    
-fig.xlabel("Longitude")
-fig.ylabel("Latitude")
-fig.colorbar()
-fig.save("Figures/Sample-Visualization.pdf")
+    im = subplots[i+1,1].imshow(difference, cmap = "turbo", vmin=vmin, vmax=vmax)
+
+fig.supxlabel("Longitude")
+fig.supylabel("Latitude")
+fig.colorbar(im, ax=subplots.ravel().tolist())
+fig.savefig("Figures/Sample-Visualization.pdf",  bbox_inches = "tight")
